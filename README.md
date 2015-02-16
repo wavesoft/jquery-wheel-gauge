055611b91de8ebcd6effbb8ff05245e5bc443352
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-02-16 10:56:52 +0100

    Adding demo page

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ jquery-wheel-gauge
 ==================
 
 A jQuery plugin for displaying a values in a more retro way.
+
+How to use
+==========
--- src/rundial.js
+++ src/rundial.js
@@ -1,4 +1,4 @@
-
+/*! .rundial() - A retro spin dial Authors & copyright (c) 2014 Ioannis Charalampidis. MIT License. */
 $.fn.rundial = function(a1,a2) {
 
 	return this.each(function(i, domElm) {
@@ -8,29 +8,60 @@ $.fn.rundial = function(a1,a2) {
 
 		// Constructor
 		if (!rundial || (action == 'create')) {
-			var cfg = a2 || {};
+			var cfg = a1 || {};
 
 			// Setup rundial config
 			rundial = {
-				'input'			: $(elm),
-				'min'			: cfg.min || 0,
-				'max'			: cfg.max || 100,
-				'width'			: cfg.width || Math.max( elm.width(), 64 ),
-				'height'		: cfg.height || Math.max( elm.height(), 64 ),
-				'step'			: cfg.step || 1,
-				'value'			: cfg.value || parseInt(elm.attr('value')) || cfg.min,
-				'overflowText'	: cfg.overflowText || '(!)',
-				'underflowText'	: cfg.underflowText || '(!)',
+
+				// Basic
+				'min'				: cfg.min    || parseFloat(elm.attr("data-min"))  || 0,
+				'max'				: cfg.max    || parseFloat(elm.attr("data-max"))  || 100,
+				'width'				: cfg.width  || 'auto',
+				'height'			: cfg.height || Math.max( elm.height(), 64 ),
+				'step'				: cfg.step   || parseFloat(elm.attr("data-step")) || 1,
+				'value'				: (cfg.value == undefined ? 
+										(elm.val() == "" ? 
+											(elm.attr('value') == undefined ?
+												cfg.min
+												: parseFloat(elm.attr('value')) )
+											: parseFloat(elm.val()))
+										: cfg.value),
+
+				// Advanced
+				'dampValue'			: cfg.dampValue         || 10,
+				'updateDelay'		: cfg.updateDelay       || 30,
+				'format'			: cfg.format            || function(v) { return String(v); },
+
+				'overflowText'		: cfg.overflowText      || '(!)',
+				'overflowElmClass'	: cfg.overflowElmClass  || 'rundial-warn',
+				'overflowClass'		: cfg.overflowClass     || 'rundial-overrun',
+
+				'underflowText'		: cfg.underflowText     || '(!)',
+				'underflowElmClass'	: cfg.underflowElmClass || 'rundial-warn',
+				'underflowClass'	: cfg.underflowClass    || 'rundial-underrun',
+
+				'adoptClasses'		: (cfg.adoptClasses == undefined) ? true : cfg.adoptClasses,
+				'ascending'			: (cfg.ascending == undefined) ? false : cfg.ascending,
+
+				// Private
+				'_lastTargetVal'	: 0,
+				'_useVal'			: $(elm).is("input"),
+				'_input'			: $(elm),
+				'_activeTimer'		: 0,
+
 			};
 
 			// Update value on the element
-			elm.attr('value', rundial.value);
+			if (rundial._useVal) {
+				rundial._input.val(rundial.value);
+			} else {
+				rundial._input.attr('value', rundial.value);
+			}
 
 			// Create & Next UI components
 			var e = $('<div class="rundial"></div>'),
 				h = $('<div class="rundial-host"></div>').appendTo(e),
-				i1= $('<div class="rundial-indicator"></div>').appendTo(h),
-				i2= $('<div class="rundial-indicator rundial-indicator-right"></div>').appendTo(h),
+				i = $('<div class="rundial-indicator-bar"></div>').appendTo(h),
 				v = [
 					$('<div class="rundial-value">-1</div>').appendTo(h),
 					$('<div class="rundial-value">0</div>').appendTo(h).css({'font-weight':'bold'}),
@@ -38,92 +69,162 @@ $.fn.rundial = function(a1,a2) {
 				];
 
 			// Style input
-			e.css({
-				'width': rundial.width,
-				'height': rundial.height
-			});
+			if ((rundial.width != 'auto') && (rundial.width)) {
+				e.css({	width: rundial.width });
+			};
+			if ((rundial.height != 'auto') && (rundial.height)) {
+				e.css({	height: rundial.height });
+			};
+
+			// Check for adopting classes
+			if (rundial.adoptClasses) {
+				e.attr("class", elm.attr("class"));
+				e.addClass("rundial");
+			}
 
 			// Update rundial data
 			rundial.container = e;
 			rundial.host = h;
 			rundial.valueElements = v;
 
-			
 			rundial.wrapStep = function(v) {
 				if (v > this.max) v = this.max;
 				if (v < this.min) v = this.min;
-				return Math.round(v / this.step) * this.step;
+				return Math.round((v-this.min) / this.step) * this.step + this.min;
 			}
+
 			rundial.setValue = function(v) {
 
-				// Bounce underflows
-				if (v < this.min) {
-					v = this.min - ((this.min - v) / (this.max - this.min)) * this.step/2;
-					if (v < this.min - this.step/2) v = this.min - this.step/2;
-					rundial.host.addClass("rundial-underrun");
-				} else {
-					rundial.host.removeClass("rundial-underrun");
-				}
-
-				// Bounce overflows
-				if (v > this.max) {
-					v = this.max + ((v - this.max) / (this.max - this.min)) * this.step/2;
-					if (v > this.max + this.step/2) v = this.max + this.step/2;
-					rundial.host.addClass("rundial-overrun");
-				} else {
-					rundial.host.removeClass("rundial-overrun");
-				}
-
-				var scale_factor = 0.5,
+				var scale_factor = 0.5, /* How hight (in % of parent) is each letter */
 					v_curr   = this.wrapStep(v),
 					v_before = v_curr - this.step,
 					v_after  = v_curr + this.step,
-					ofs = this.height*( 1 - scale_factor*(1 - 2*v_curr + 2*v) )/2;
-					
+					ofs = ( this.ascending ? (v_curr - v) : (v - v_curr) )/this.step,
+					y0 = this.height*(1 - scale_factor)/2,
+					y = y0 + ofs*this.height*scale_factor;
+				
+				// Set positions
+				var iTop=0, iBottom=2;
+				if (!this.ascending) {
+					iTop=2, iBottom=0;
+				}
+
+				if (this.ascending) {
+					this.valueElements[iTop].css({ 
+						top: y - this.height*scale_factor 
+					});
+					this.valueElements[iBottom].css({ 
+						top: y + this.height*scale_factor 
+					});
+				} else {
+					this.valueElements[iTop].css({ 
+						top: y + this.height*scale_factor 
+					});
+					this.valueElements[iBottom].css({ 
+						top: y - this.height*scale_factor 
+					});
+				}
+				this.valueElements[1].css({ 
+					top: y 
+				});
+
 				// Set texts
 				if (v_before < this.min) {
 					if (v < this.min) {
-						this.valueElements[0].html( this.underflowText );
-						this.valueElements[0].addClass("rundial-warn");
+						this.valueElements[iTop].html( this.underflowText );
+						this.valueElements[iTop].addClass( this.underflowElmClass );
 					} else {
-						this.valueElements[0].text("");
-						this.valueElements[0].removeClass("rundial-warn");
+						this.valueElements[iTop].text("");
+						this.valueElements[iTop].removeClass( this.underflowElmClass );
 					}
 				} else {
-					this.valueElements[0].text(v_before);
-					this.valueElements[0].removeClass("rundial-warn");
+					this.valueElements[iTop].text( this.format(v_before) );
+					this.valueElements[iTop].removeClass( this.underflowElmClass );
 				}
-				this.valueElements[1].text(v_curr);
 				if (v_after > this.max) {
 					if (v > this.max) {
-						this.valueElements[2].html( this.overflowText );
-						this.valueElements[2].addClass("rundial-warn");
+						this.valueElements[iBottom].html( this.overflowText );
+						this.valueElements[iBottom].addClass( this.overflowElmClass );
 					} else {
-						this.valueElements[2].text("");
-						this.valueElements[2].removeClass("rundial-warn");
+						this.valueElements[iBottom].text("");
+						this.valueElements[iBottom].removeClass( this.overflowElmClass );
 					}
 				} else {
-					this.valueElements[2].text(v_after);
-					this.valueElements[2].removeClass("rundial-warn");
-				}
+					this.valueElements[iBottom].text( this.format(v_after) );
+					this.valueElements[iBottom].removeClass( this.overflowElmClass );
+				}				
+				this.valueElements[1].text( this.format(v_curr) );
 
-				this.valueElements[0].css({ top: ofs - this.height*scale_factor });
-				this.valueElements[1].css({ top: ofs });
-				this.valueElements[2].css({ top: ofs + this.height*scale_factor });
+				// Set styles
+				if (v < this.min) {
+					rundial.host.addClass( this.underflowClass );
+				} else {
+					rundial.host.removeClass( this.underflowClass );
+				}
+				if (v > this.max) {
+					rundial.host.addClass( this.overflowClass );
+				} else {
+					rundial.host.removeClass( this.overflowClass );
+				}
 
 			}
 			rundial.tick = function() {
-				var targetVal = parseInt(elm.attr('value'));
+				
+				// Find the value we should target
+				var targetVal;
+				if (this._useVal) {
+					targetVal = parseFloat(this._input.val());
+				} else {
+					targetVal = parseFloat(this._input.attr('value'));
+				}
+
+				// Use previous target if found invalid something
+				if (isNaN(targetVal)) targetVal = this._lastTargetVal;
+				this._lastTargetVal = targetVal;
+
+				// Bounce underflows
+				if (targetVal < this.min) {
+					targetVal = this.min - ((this.min - targetVal) / (this.max - this.min)) * this.step/2;
+					if (targetVal < this.min - this.step/2) targetVal = this.min - this.step/2;
+				} 
+
+				// Bounce overflows
+				else if (targetVal > this.max) {
+					targetVal = this.max + ((targetVal - this.max) / (this.max - this.min)) * this.step/2;
+					if (targetVal > this.max + this.step/2) targetVal = this.max + this.step/2;
+				}
+
+				// Apply it graduately
 				if (Math.abs(targetVal - this.value) > 0.01) {
-					this.value += (targetVal - this.value) / 20;
-					rundial.setValue(this.value);
+					this.value += (targetVal - this.value) / this.dampValue;
+					this.setValue(this.value);
 				} else if (targetVal != this.value) {
 					this.value = targetVal;
-					rundial.setValue(this.value);
+					this.setValue(this.value);
+					// Stop timer
+					clearInterval(this._activeTimer);
+					this._activeTimer = 0;
 				}
+
+			}
+			rundial.startTimer = function() {
+				var self = this;
+				if (this._activeTimer)
+					clearInterval(this._activeTimer);
+
+				// Start interval
+				this._activeTimer = setInterval(
+					function() {  self.tick(); },
+					rundial.updateDelay
+				);
 			}
 
-			setInterval(function() { rundial.tick(); }, 25);
+			// Trigger startTimer on change
+			elm.change(function() {
+				rundial.startTimer();
+			});
+
+			// Set dial value
 			rundial.setValue( rundial.value );
 
 			// Store rundial data
@@ -135,6 +236,32 @@ $.fn.rundial = function(a1,a2) {
 
 		}
 
+		else if (action == 'value') {
+
+			if (a2 == undefined) {
+				
+				// Get value
+				var targetVal;
+				if (rundial._useVal) {
+					targetVal = parseFloat(rundial._input.val());
+				} else {
+					targetVal = parseFloat(rundial._input.attr('value'));
+				}
+				return targetVal;
+
+			} else {
+
+				// Set value
+				if (rundial._useVal) {
+					rundial._input.val(a2);
+				} else {
+					rundial._input.attr('value', a2);
+				}
+				rundial.startTimer();
+
+			}
+
+		}
 
 	});
 };
